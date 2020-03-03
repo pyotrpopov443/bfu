f2fc841c22df6201dd16c5739b61b764a8bfe5bf
petrpopov <pyotr.popov443@mail.ru>
2020-03-03 13:20:22 +0200

    view pager

--- app/src/main/java/com/example/bfusummerschool/MainActivity.java
+++ app/src/main/java/com/example/bfusummerschool/MainActivity.java
@@ -51,6 +51,7 @@ public class MainActivity extends AppCompatActivity{
                     }
                 });
 
+
         viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
